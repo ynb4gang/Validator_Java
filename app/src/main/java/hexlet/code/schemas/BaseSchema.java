7707d53f6e2c4a032aee0e4049d778e20db39b0c
ynb4gang <129555715+ynb4gang@users.noreply.github.com>
2024-03-20 14:14:38 +0300

    first update

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -3,11 +3,18 @@ package hexlet.code.schemas;
 public abstract class BaseSchema<T> {
     protected boolean required = false;
 
-    public abstract boolean isValid(T value);
-
     public BaseSchema<T> required() {
         required = true;
         return this;
     }
-}
 
+    public abstract boolean isValid(T value);
+
+    public BaseSchema<T> minLength(int minLength) {
+        return this;
+    }
+
+    public BaseSchema<T> contains(String substring) {
+        return this;
+    }
+}
